c9d13022624e0f200dac5f2cce19f6827e43a2a4
Haythem532002 <khiarih06@gmail.com>
2024-06-05 17:47:08 +0100

    adding test

--- src/Create.test.tsx
+++ src/Create.test.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import Create from "./Components/Create";
 
 test("renders from inputs and submit button", () => {
   render(<Create />);
-  const emailInput = screen.getByLabelText("/email/i") as HTMLInputElement;
-  const passwordInput = screen.getByLabelText(
-    "/password/i"
-  ) as HTMLInputElement;
+  const emailInput = screen.getByLabelText("Email") as HTMLInputElement;
+  const passwordInput = screen.getByLabelText("Password") as HTMLInputElement;
   const experienceInput = screen.getByLabelText(
-    "/experience/i"
+    "Year of experience"
   ) as HTMLSelectElement;
-  const submitButton = screen.getByRole("button", { name: /create account/i });
+  const submitButton = screen.getByRole("button", { name: "Create Account" });
 
   expect(emailInput).toBeInTheDocument();
   expect(passwordInput).toBeInTheDocument();
@@ -20,13 +19,19 @@ test("renders from inputs and submit button", () => {
 
   fireEvent.change(emailInput, { target: { value: "haythem05@ieee.org" } });
   fireEvent.change(passwordInput, { target: { value: "haythem123" } });
-  fireEvent.change(experienceInput, { target: { value: 1 } });
+  fireEvent.change(experienceInput, { target: { value: "1" } });
 
   expect(emailInput.value).toBe("haythem05@ieee.org");
-  expect(passwordInput.value).toBe("password123");
-  expect(experienceInput).toBe(1);
+  expect(passwordInput.value).toBe("haythem123");
+  expect(experienceInput.value).toBe("1");
 
   fireEvent.click(submitButton);
 
-  expect(window.alert).toHaveBeenCalledWith("Form Submitted Succesfully");
+  const alertMock = jest.spyOn(window, "alert").mockImplementation(() => {});
+
+  fireEvent.click(submitButton);
+
+  expect(alertMock).toHaveBeenCalledWith("Form Submitted Succesfully");
+
+  alertMock.mockRestore();
 });
